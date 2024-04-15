210d72f8e59af07aa1e805b94eafe8855123d9fe
mpatel2280 <patelm.2280@gmail.com>
2024-04-15 17:48:04 +0530

    Symfony 7 webapp

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ProductController extends AbstractController
+{
+    // #[Route('/product', name: 'app_product')]
+    // public function index(): Response
+    // {
+    //     return $this->render('product/index.html.twig', [
+    //         'controller_name' => 'ProductController',
+    //     ]);
+    // }
+
+    #[Route('/product', name: 'create_product')]
+    public function createProduct(EntityManagerInterface $entityManager): Response
+    {
+        $product = new Product();
+        $product->setName('Keyboard');
+        $price = random_int(0, 10000);
+        $product->setPrice($price);
+        $product->setDescription('Ergonomic and stylish!');
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
+    }
+
+    #[Route('/product/{id}', name: 'product_show')]
+    public function show(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        return new Response('Check out this great product: '.$product->getName() . ' at ' . $product->getPrice());
+
+        // or render a template
+        // in the template, print things with {{ product.name }}
+        // return $this->render('product/show.html.twig', ['product' => $product]);
+    }
+}
